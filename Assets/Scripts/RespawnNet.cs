82ba22364982d8348fab326e71ae00f9f56cb543
ARigitano <antoine@rigitano.com>
2025-07-15 20:04:58 +0200

    Creating a respawn net

---
+++ Assets/Scripts/RespawnNet.cs
@@ -0,0 +1,19 @@
+﻿using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+//Respawns players on the asteroid if they fall off the asteroid.
+public class RespawnNet : UdonSharpBehaviour
+{
+    [SerializeField]
+    private Transform _spawnPoint; //Position at which the player will be respawned.
+    [SerializeField]
+
+    public override void OnPlayerTriggerEnter(VRCPlayerApi player)
+    {
+        base.OnPlayerTriggerEnter(player);
+
+        player.TeleportTo(_spawnPoint.position, Quaternion.identity);
+    }
+}
