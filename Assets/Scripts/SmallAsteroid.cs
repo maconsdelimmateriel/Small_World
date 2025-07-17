eb58d250678a2cf41b0e6cb4a8774e6301b09b7d
ARigitano <antoine@rigitano.com>
2025-07-17 17:18:15 +0200

    Orbiting smaller asteroids

---
+++ Assets/Scripts/SmallAsteroid.cs
@@ -0,0 +1,60 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+//Script attached to the basic small asteroid.
+public class SmallAsteroid : UdonSharpBehaviour
+{
+    [Header("Orbit Center")]
+    public Transform orbitCenter;
+
+    [Header("Orbit Shape")]
+    public float semiMajorAxis = 5f;
+    public float semiMinorAxis = 3f;
+
+    [Header("Orbit Speed")]
+    public float baseOrbitSpeed = 1f;
+    public bool randomizeSpeed = true;
+    public float minSpeed = 0.5f;
+    public float maxSpeed = 2f;
+
+    [Header("Orbit Inclination (Rotation in Degrees)")]
+    public Vector3 orbitTiltEuler = new Vector3(0f, 0f, 0f); // X, Y, Z tilt
+
+    [Header("Start Angle")]
+    public float startAngle = 0f;
+    public bool randomizeStartAngle = true;
+
+    private float angle;
+    private float orbitSpeed; // final speed used
+
+    void OnEnable()
+    {
+        angle = randomizeStartAngle ? Random.Range(0f, Mathf.PI * 2f) : startAngle;
+        orbitSpeed = randomizeSpeed ? Random.Range(minSpeed, maxSpeed) : baseOrbitSpeed;
+    }
+
+    void Update()
+    {
+        if (orbitCenter == null) return;
+
+        // Advance angle over time
+        angle += orbitSpeed * Time.deltaTime;
+        angle %= Mathf.PI * 2f;
+
+        // Compute position on ellipse
+        float x = semiMajorAxis * Mathf.Cos(angle);
+        float z = semiMinorAxis * Mathf.Sin(angle);
+        Vector3 localPos = new Vector3(x, 0f, z);
+
+        // Apply 3D rotation (orbit inclination)
+        Quaternion tiltRotation = Quaternion.Euler(orbitTiltEuler);
+        Vector3 rotatedPos = tiltRotation * localPos;
+
+        // Final world position
+        transform.position = orbitCenter.position + rotatedPos;
+    }
+}
+
---
+++ Assets/Scripts/SmallAsteroidsManager.cs
@@ -0,0 +1,63 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+using System.Collections.Generic;
+
+public class SmallAsteroidsManager : UdonSharpBehaviour
+{
+    [Header("Manually Assigned Asteroids (Disabled in Scene)")]
+    [SerializeField] private GameObject[] _asteroidPool; //All the small asteroids.
+
+    [Header("Activation Settings")]
+    public float activationInterval = 1f; //Interval at which a new small asteroid appears.
+
+    private float _timer;
+
+    void Update()
+    {
+        _timer += Time.deltaTime;
+
+        if (_timer >= activationInterval)
+        {
+            _timer = 0f;
+            SendCustomNetworkEvent(VRC.Udon.Common.Interfaces.NetworkEventTarget.All, "ActivateAsteroidFromPool");
+        }
+    }
+
+    //Activates an asteroid.
+    public void ActivateAsteroidFromPool()
+    {
+        foreach (GameObject asteroid in _asteroidPool)
+        {
+            if (!asteroid.activeInHierarchy)
+            {
+                SmallAsteroid orbit = asteroid.GetComponent<SmallAsteroid>();
+                if (orbit != null)
+                {
+                    orbit.randomizeStartAngle = true;
+                    orbit.randomizeSpeed = true;
+                    orbit.orbitTiltEuler = new Vector3(
+                        Random.Range(-30f, 30f),
+                        Random.Range(0f, 360f),
+                        0f
+                    );
+                }
+
+                asteroid.SetActive(true);
+                break;
+            }
+        }
+    }
+
+    //Deactivates an asteroid. Will probably be used when all the fuel in an asteroid has been depleted.
+    /*public void ReturnToPool(GameObject asteroid)
+    {
+        if (asteroidPool.Contains(asteroid))
+        {
+            asteroid.SetActive(false);
+        }
+    }
+    */
+}
