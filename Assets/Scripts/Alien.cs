853fd8b16a10efe45db231854377fa4ce2b4f7b5
ARigitano <antoine@rigitano.com>
2025-08-17 01:58:27 +0200

    Alien pod prefab & script

--- Assets/Scripts/Alien.cs
+++ Assets/Scripts/Alien.cs
@@ -1,18 +1,38 @@
-using System.Collections;
-using System.Collections.Generic;
+﻿
+using UdonSharp;
 using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+using TMPro;
 
-public class Alien : MonoBehaviour
+public class Alien : UdonSharpBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private UnityEngine.Object[] _dialogLines;
+    [SerializeField] private GameObject _canvas;
+    [SerializeField] private TextMeshProUGUI _text;
+
+    public void StartDialog()
     {
-        
+        _canvas.SetActive(true);
+        //_text.text = _dialogLines[0].lineFrench;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void EndDialog()
     {
-        
+        _canvas.SetActive(false);
+    }
+
+    public override void OnPlayerTriggerEnter(VRCPlayerApi player)
+    {
+        base.OnPlayerTriggerEnter(player);
+
+        SendCustomNetworkEvent(VRC.Udon.Common.Interfaces.NetworkEventTarget.All, "StartDialog");
+    }
+
+    public override void OnPlayerTriggerExit(VRCPlayerApi player)
+    {
+        base.OnPlayerTriggerEnter(player);
+
+        SendCustomNetworkEvent(VRC.Udon.Common.Interfaces.NetworkEventTarget.All, "EndDialog");
     }
 }
