b0c034b8c8308cca900c3e7cd43ad70c63f46521
ARigitano <antoine@rigitano.com>
2025-12-08 21:16:31 +0100

    Putting fuel in heater

---
+++ Assets/Scripts/Asteroids/SmallAsteroidFuelPrefab.cs
@@ -0,0 +1,24 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+//Script for the small asteroid when in its caught state.
+public class SmallAsteroidFuelPrefab : UdonSharpBehaviour
+{
+    //Destroys the asteroid when put in the heater.
+    public void DestroyShell()
+    {
+        Destroy(gameObject);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.GetComponent<Heater>() != null)
+        {
+            other.GetComponent<Heater>().ReceiveFuelActivate();
+            SendCustomNetworkEvent(VRC.Udon.Common.Interfaces.NetworkEventTarget.All, "DestroyShell");
+        }
+    }
+}
---
+++ Assets/Scripts/Heater.cs
@@ -0,0 +1,20 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+//Script for the heater that warms the big asteroid.
+public class Heater : UdonSharpBehaviour
+{
+    public void ReceiveFuelActivate()
+    {
+        SendCustomNetworkEvent(VRC.Udon.Common.Interfaces.NetworkEventTarget.All, "ReceiveFuel");
+    }
+
+    //Called when fuel is added to the heater.
+    public void ReceiveFuel()
+    {
+        Debug.Log("fuel");
+    }
+}
