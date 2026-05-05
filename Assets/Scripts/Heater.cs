d022740328e83cb85e89a2ab8b6f61fb7e97802f
ARigitano <antoine@rigitano.com>
2026-05-05 21:26:56 +0200

    Adapting heater methods to new UI

--- Assets/Scripts/Heater.cs
+++ Assets/Scripts/Heater.cs
@@ -4,13 +4,15 @@ using UnityEngine;
 using VRC.SDKBase;
 using VRC.Udon;
 using TMPro;
+using UnityEngine.UI;
 
 //Script for the heater that warms the big asteroid.
 public class Heater : UdonSharpBehaviour
 {
-    [SerializeField] private TextMeshProUGUI _uiText; //UI placeholder that displays how much fuel is left in the heater.
-    [SerializeField] private float _fuelBonus = 10f; //Placeholver number of how much fuel is added each time an asteroid is inserted in the heater.
-    [UdonSynced] private float _counter = 0f; //Counts how much fuel is left in the heater.
+    [SerializeField] private Slider _uiSlider; //UI that displays how much fuel is left in the heater.
+    [SerializeField] private float _fuelBonus = 0.2f; //Placeholver number of how much fuel is added each time an asteroid is inserted in the heater.
+    [SerializeField] private float _fuelLoss = 0.01f; //How much fuel is depleted each second.
+    [UdonSynced] [SerializeField] private float _counter = 0f; //Counts how much fuel is left in the heater.
     private float _timer = 0f; //Timer for decreasing the fuel counter.
 
 
@@ -29,7 +31,7 @@ public class Heater : UdonSharpBehaviour
     //Updates the UI each time the fuel decreases by 1.
     public void UpdateCounterText()
     {
-        _uiText.text = _counter.ToString();
+        _uiSlider.value = _counter;
     }
 
     private void Update()
@@ -40,7 +42,7 @@ public class Heater : UdonSharpBehaviour
 
             if (_timer >= 1f)
             {
-                _counter--;
+                _counter -= _fuelLoss;
                 RequestSerialization();
 
                 SendCustomNetworkEvent(VRC.Udon.Common.Interfaces.NetworkEventTarget.All, "UpdateCounterText");
